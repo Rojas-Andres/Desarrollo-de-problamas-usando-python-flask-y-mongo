cb2629d5cbbbbadbf1ea5736ead1a5163d05754c
RojasxAndres <afrojaslondono@gmail.com>
2020-08-06 12:16:26 -0500

    Creando rutas

--- App.py
+++ App.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
     return 'Bienvenido'
+@app.route('/add_contact')
+def add_contact():
+    return 'Añadir contacto'
+@app.route('/edit_contact')
+def edit_contact():
+    return 'Editar contacto'
+@app.route('/delete')
+def delete_contact():
+    return 'Borrar contacto'
+
 #El debug significa que cada vez que hagamos un cambio se reinicie el server
 if __name__== '__main__': 
     app.run(port = 3000 ,debug = True)
