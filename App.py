8c4321d3af2844b0b4a1e64ad542445c367b606c
RojasxAndres <afrojaslondono@gmail.com>
2020-08-06 18:29:33 -0500

    Conectando a la tabla clientes con sus respectivos insert

--- App.py
+++ App.py
@@ -1,20 +1,35 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_mysqldb import MySQL
 #Esta seria la clase que vamos a configurar
 app = Flask(__name__)
-app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_HOST']='127.0.0.1'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='flaskcontact'
+app.config['MYSQL_DB']='flask_contact'
 #Conexion a la base de datos
 mysql = MySQL(app)
 #Decorador cada vez que un usuario entre a la ruta principal le responda algo
 @app.route('/')
 def Index():
     return render_template('index.html')
-@app.route('/add_contact')
+@app.route('/add_contact',methods=['POST'])
 def add_contact():
-    return 'Añadir contacto'
+    if request.method == 'POST':
+        #Nos referimos al name del formulario del index
+        nombre = request.form["Nombre"]
+        cedula = request.form["Cedula"]
+        direccion = request.form["Direccion"]
+        telefono = request.form["Telefono"]
+        foto = request.form["Foto"]
+        #print(nombre)
+        
+        #Establecemos un cursor para la conexion a la bd
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO clientes (nombre,cedula,direccion,telefono,foto) values (%s,%s,%s,%s,%s)", (nombre,cedula,direccion,telefono,foto))
+        #cur.execute("Select * from contacts")
+        mysql.connection.commit()
+        
+        return "Recibido"
 @app.route('/edit_contact')
 def edit_contact():
     return 'Editar contacto'
