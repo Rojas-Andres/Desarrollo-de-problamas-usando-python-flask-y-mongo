b9692d625e5ebaad3497dc05ae5ccb627aa03dd0
RojasxAndres <afrojaslondono@gmail.com>
2020-08-06 23:58:41 -0500

    Se añaden los productos

--- App.py
+++ App.py
@@ -19,13 +19,17 @@ app.secret_key="mysecretkey"
 mysql = MySQL(app)
 #Decorador cada vez que un usuario entre a la ruta principal le responda algo
 @app.route('/')
-def Index():
+def index():
+    return render_template('index.html' )
+
+@app.route('/contactos')
+def contactos():
     #Realizamos la consulta a la base de datos para enviar los clientes
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM CLIENTES')
     data = cur.fetchall()
     #Le envio los clientes al index para que los muestre 
-    return render_template('index.html',clientes = data )
+    return render_template('contactos.html',clientes = data )
 
 @app.route('/add_cliente',methods=['POST'])
 def add_cliente():
@@ -45,7 +49,7 @@ def add_cliente():
         #Flash permite enviar mensajes entre vistas
         flash("Contacto agregado satisfactoriamente")
         #Redireccionamos a la principal
-        return redirect(url_for('Index'))
+        return redirect(url_for('contactos'))
 
 @app.route('/edit_contact/<int:cedula>')
 def get_cliente(cedula):
@@ -68,7 +72,7 @@ def actualizar(cedula):
      """,(cedula_cli,nombre,direccion,telefono,foto,cedula))
     mysql.connection.commit()
     flash("El contacto fue actualizado satisfactoriamente")
-    return redirect(url_for('Index')) 
+    return redirect(url_for('contactos')) 
 
 #Le envio la cedula 
 @app.route('/delete/<int:cedula>')
@@ -77,7 +81,74 @@ def delete_cliente(cedula):
     cur.execute("DELETE FROM CLIENTES WHERE cedula = {0} ".format(cedula))
     mysql.connection.commit()
     flash("Contacto borrado satisfactoriamente")
-    return redirect(url_for('Index'))
+    return redirect(url_for('contactos'))
+
+
+#Productos
+
+@app.route('/productos')
+def productos():
+    #Realizamos la consulta a la base de datos para enviar los clientes
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM PRODUCTOS')
+    data = cur.fetchall()
+    #Le envio los clientes al index para que los muestre 
+    return render_template('productos.html',productos = data )
+
+@app.route('/add_productos',methods=['POST'])
+def add_productos():
+    if request.method == 'POST':
+        #Nos referimos al name del formulario del index
+        codigo = request.form["Codigo"]
+        categoria = request.form["Categoria"]
+        nombre = request.form["Nombre"]
+        precio = request.form["Precio"]
+        cantidad = request.form["Cantidad"]
+        estado = request.form["Estado"]
+        #Establecemos un cursor para la conexion a la bd
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO productos (codigo_producto ,categoria,nombre ,precio,cantidad_bodega,estado) values (%s,%s,%s,%s,%s,%s)", (codigo,categoria,nombre,precio,cantidad,estado))
+        #cur.execute("Select * from contacts")
+        mysql.connection.commit()
+        #Flash permite enviar mensajes entre vistas
+        flash("Producto ha sido agregado satisfactoriamente")
+        #Redireccionamos a la principal
+        return redirect(url_for('productos'))
+
+@app.route('/edit_producto/<int:producto>')
+def get_producto(producto):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM PRODUCTOS WHERE codigo_producto = {0} ".format(producto))
+    dato = cur.fetchall()
+    return render_template('editar_producto.html',producto = dato[0] )
+
+@app.route('/actualizar_producto/<int:producto>',methods=['POST'])
+def actualizar_producto(producto):
+    cur = mysql.connection.cursor()
+    codigo = request.form["Codigo"]
+    categoria = request.form["Categoria"]
+    nombre = request.form["Nombre"]
+    precio = request.form["Precio"]
+    cantidad = request.form["Cantidad"]
+    estado = request.form["Estado"]
+    cur.execute("""
+    UPDATE PRODUCTOS SET codigo_producto = %s ,categoria =%s,nombre =%s,precio =%s,cantidad_bodega=%s ,estado=%s
+    where codigo_producto=%s
+     """,(codigo,categoria,nombre,precio,cantidad,estado,producto))
+    mysql.connection.commit()
+    flash("El producto fue actualizado satisfactoriamente")
+    return redirect(url_for('productos')) 
+
+
+#Le envio el producto 
+@app.route('/delete_producto/<int:producto>')
+def delete_producto(producto):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM PRODUCTOS WHERE codigo_producto = {0} ".format(producto))
+    mysql.connection.commit()
+    flash("Producto borrado satisfactoriamente")
+    return redirect(url_for('productos'))
+
 #El debug significa que cada vez que hagamos un cambio se reinicie el server
 if __name__== '__main__': 
     app.run(port = 3000 ,debug = True)
